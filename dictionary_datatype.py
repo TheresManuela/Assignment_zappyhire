a072723b2189a73024668f516fafba60a5351b21
TheresManuela <theresnormal@gmail.com>
2024-02-12 01:30:13 +0530

    push to Assignment2

---
+++ dictionary_datatype.py
@@ -0,0 +1,12 @@
+def dict(list):
+    result_dict={}
+    for item in list:
+        data_type=type(item)
+        if data_type in result_dict:
+            result_dict[data_type].append(item)
+        else:
+            result_dict[data_type]=[item]
+    return result_dict
+input=[34,"word",4,5,"code",89,9.0]
+result_dict=dict(input)
+print(result_dict)
